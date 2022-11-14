c1dc7469f8b67c3bb0ee4b9d7904c112ca3e99b4
Lucas Genovese Desktop <lucas59311@gmail.com>
2022-11-14 20:56:27 -0300

    Input handling for API

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Data from './components/Data';
 function App() {
   const [data, setData] = useState({});
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   const [details,setDetails] = useState({
     webTradeEligibility: "",
@@ -25,11 +26,22 @@ function App() {
     e.preventDefault();
     const url = `http://localhost:3001/game-list?webTradeEligibility=${details.webTradeEligibility}&browserid=${details.browserid}&steamLoginSecure=${details.steamLoginSecure}&sessionid=${details.sessionid}&steamparental=${details.steamparental}`
 
-    axios.get(url).then((response) => {
+    axios.get(url)
+    .then(response => {
       setData(response.data);
       setLoading(true);
-      console.log(response.data)
-    })
+    }).catch(error => {
+      if (error.response){
+        setError('Could not get any games for that input. Try loading your cookies again');
+        console.log('Could not get any games for that input. Try loading your cookies again');
+      } else if (error.request){
+        setError('API is currently down');
+        console.log('API is currently down');
+      } else {
+        setError('Unespected error: ', error.message);
+        console.log('Unespected error: ', error.message);
+      }
+    })    
   }
 
   const dataList = loading && data.map(data => (<Data data={data}></Data>))
@@ -48,6 +60,8 @@ function App() {
         <button type="submit">Submit</button>
       </form>
       
+      {error && <h2>{error}</h2>}
+
       {loading ? <table className="center">
         <tbody>
           <tr>
@@ -59,7 +73,7 @@ function App() {
           </tr>  
         </tbody>
         {dataList}
-      </table> : <h1>loading...</h1>}
+      </table> : null}
     
 
     </div>
